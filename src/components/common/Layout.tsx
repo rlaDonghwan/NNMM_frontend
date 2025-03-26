a240b09e5aa9976edadb3f2b96d64744f7bcccff
JihyunKim <kimwlgus001130@gmail.com>
2025-03-26 17:51:42 +0900

    상단 navBar

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import {Button} from '@/components/ui/button'
+import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar'
+import React from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  NavigationMenuLink
+} from '../ui/navigation-menu'
+
+export default function Layout({children}) {
+  return (
+    <>
+      <NavigationMenu className="justify-between min-w-full p-4 bg-white shadow">
+        <NavigationMenuList className="text-2xl font-bold">
+          <img
+            src="/images/Dashboard.png"
+            alt="NNMM"
+            className="inline-block w-8 h-8 mr-2"
+          />
+          NNMM
+        </NavigationMenuList>
+        <NavigationMenuList>
+          <Link href="/auth/signin" legacyBehavior passHref>
+            <Button>로그아웃</Button>
+          </Link>
+          <Avatar className="ml-4">
+            <AvatarImage src="https://avatars.githubusercontent.com/u/118759932?v=4&size=64" />
+            <AvatarFallback>CNs</AvatarFallback>
+          </Avatar>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
+  )
+}
