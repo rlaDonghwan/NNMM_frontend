098b8ea2f48a5c6df1e926414858a4a93308f8b7
JihyunKim <kimwlgus001130@gmail.com>
2025-03-28 18:03:50 +0900

    new navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,20 @@ import type {AppProps} from 'next/app'
 import {Toaster} from 'react-hot-toast'
 import Layout from '@/components/common/Layout'
 import {useRouter} from 'next/router'
+import type {NextPage} from 'next'
+import type {ReactElement, ReactNode} from 'react'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
 
 // 메인 애플리케이션 컴포넌트를 정의합니다.
-export default function MyApp({Component, pageProps}: AppProps) {
+export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
   const router = useRouter()
-
+  const getLayout = Component.getLayout ?? (page => page)
   // Layout이 필요 없는 페이지 경로를 정의합니다.
   const noLayoutPages = ['/auth/signin', '/auth/signup', '/main/main']
   const isNoLayoutPage = noLayoutPages.includes(router.pathname)
@@ -18,8 +27,6 @@ export default function MyApp({Component, pageProps}: AppProps) {
       <Component {...pageProps} />
     </>
   ) : (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    getLayout(<Component {...pageProps} />)
   )
 }
