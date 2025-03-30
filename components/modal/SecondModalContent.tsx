29cc198533baf83c909fced23c46a1ac575a68e2
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-03-30 17:10:57 +0900

    modalTailwindCSS 3

--- components/modal/SecondModalContent.tsx
+++ components/modal/SecondModalContent.tsx
@@ -135,7 +135,7 @@ export default function SecondModalContent({
           </div>
         </div>
 
-        {/* 그래프 미리보기 */}
+        {/* 그래프 미리보기 ,,,*/}
         <div className="col-span-1 space-y-2">
           <h3 className="font-medium">그래프 미리보기</h3>
           <div className="bg-gray-100 rounded-xl h-64 flex items-center justify-center">
