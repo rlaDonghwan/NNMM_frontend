e3dfd3bc990f638e6a7241497b6ba2758a25223a
JihyunKim <kimwlgus001130@gmail.com>
2025-04-04 18:59:24 +0900

    메인대시보드 수정

--- app/(bar)/dashboard/page.tsx
+++ app/(bar)/dashboard/page.tsx
@@ -1,13 +1,19 @@
+'use client'
+
 import DashboardGrid from '@/components/dashboard/TotalDashboard'
-import {fetchCurrentUser} from 'services/auth'
-import {toast} from 'react-hot-toast'
-import {GetServerSideProps} from 'next'
+import {ESGModalProvider} from '@/components/modal/ESGModalContext'
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
 
 // 대시보드 페이지
 export default function DashboardPage() {
   return (
-    <div className="flex h-full w-full bg-white px-4 py-8">
-      <DashboardGrid />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <ESGModalProvider>
+        <div className="flex h-full w-full bg-white px-4 py-8">
+          <DashboardGrid />
+        </div>
+      </ESGModalProvider>
+    </DndProvider>
   )
 }
