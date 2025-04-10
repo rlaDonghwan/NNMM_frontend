8f048d9774f9f6df3bd6974dc94eaa9a0b8a4dec
donghwan <kdhe9797@naver.com>
2025-04-10 15:50:33 +0900

    대시보드 복구

---
+++ app/(bar)/dashboard/environmental/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Environmental from '@/components/dashboard/environmental'
+import ESGModal from '@/components/modal/UnifiedESGModal'
+import {ESGModalProvider} from '@/components/modal/ESGModalContext'
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+
+export default function environmental() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ESGModalProvider>
+        <div className="flex h-full w-full px-4 py-8">
+          {/* className="flex h-full w-full bg-white px-4 py-8"로 수정------------------------------ */}
+          <Environmental />
+        </div>
+        <ESGModal category="environmental" />
+      </ESGModalProvider>
+    </DndProvider>
+  )
+}
---
+++ app/(bar)/dashboard/governance/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Governance from '@/components/dashboard/governance'
+import ESGModal from '@/components/modal/UnifiedESGModal'
+import {ESGModalProvider} from '@/components/modal/ESGModalContext'
+import {DndProvider} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+
+export default function governance() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ESGModalProvider>
+        <div className="flex h-full w-full px-4 py-8">
+          {/* className="flex h-full w-full bg-white px-4 py-8"로 수정------------------------------ */}
+          <Governance />
+        </div>
+        <ESGModal category="governance" />
+      </ESGModalProvider>
+    </DndProvider>
+  )
+}
